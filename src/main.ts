372c8509f93d424e0ea104b93080606acb606f66
def-SpaceWar <aryanahire9086360780@gmail.com>
2022-10-30 13:09:31 -0400

    Got 2D working

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,46 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import { setupCounter } from './counter'
+import './style.css';
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+import { GLInstance, ShaderUtil } from './webglsetup';
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+const _data = GLInstance()!,
+    gl = _data.gl,
+    clear = _data.clear,
+    setSize = _data.setSize;
+
+//const WIDTH = gl.canvas.width;
+//const HEIGHT = gl.canvas.height;
+
+setSize(window.innerWidth, window.innerHeight);
+window.onresize = () => { setSize(window.innerWidth, window.innerHeight); };
+clear();
+
+const shaderProgram = ShaderUtil.createProgram(gl)!;
+
+gl.useProgram(shaderProgram);
+const aPositionLocation = gl.getAttribLocation(shaderProgram, "a_position"),
+    uPointSizeLocation = gl.getUniformLocation(shaderProgram, "uPointSize");
+gl.useProgram(null);
+
+const arrVerts = new Float32Array([0, 0, 0, 0.5, 0.5, 0.5]),
+    bufVerts = gl.createBuffer();
+
+gl.bindBuffer(gl.ARRAY_BUFFER, bufVerts);
+gl.bufferData(gl.ARRAY_BUFFER, arrVerts, gl.STATIC_DRAW);
+gl.bindBuffer(gl.ARRAY_BUFFER, null);
+
+gl.useProgram(shaderProgram);
+gl.uniform1f(uPointSizeLocation, 50.0);
+
+gl.bindBuffer(gl.ARRAY_BUFFER, bufVerts);
+gl.enableVertexAttribArray(aPositionLocation);
+gl.vertexAttribPointer(aPositionLocation, 3, gl.FLOAT, false, 0, 0);
+gl.bindBuffer(gl.ARRAY_BUFFER, null);
+
+requestAnimationFrame(loop);
+function loop() {
+    clear();
+
+    gl.drawArrays(gl.POINTS, 0, 2);
+
+    requestAnimationFrame(loop);
+};
---
+++ src/webglsetup.ts
@@ -0,0 +1,68 @@
+const glClear = (gl: WebGL2RenderingContext) => () => {
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+};
+
+const glSetSize = (gl: WebGL2RenderingContext) => (w: number, h: number) => {
+    gl.canvas.style.width = w + "px";
+    gl.canvas.style.height = h + "px";
+    gl.canvas.width = w;
+    gl.canvas.height = h;
+    gl.viewport(0, 0, w, h);
+}
+
+export const GLInstance = () => {
+    const gl = document.getElementsByTagName("canvas")[0].getContext("webgl2");
+    if (!gl) { console.error("WebGL2 Context not available."); return null; }
+    gl.clearColor(0.5, 0.5, 0.5, 1);
+    return {gl, clear: glClear(gl), setSize: glSetSize(gl)};
+}
+
+import vertexShaderSource from './vertex_shader.glsl?raw';
+import fragmentShaderSource from './fragment_shader.glsl?raw';
+
+export class ShaderUtil {
+    private static createShader(gl: WebGL2RenderingContext, src: string, type: number) {
+        const shader = gl.createShader(type)!;
+        gl.shaderSource(shader, src);
+        gl.compileShader(shader);
+        if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+            console.error("Error compiling shader:\n" + src, gl.getShaderInfoLog(shader));
+            gl.deleteShader(shader);
+            return null;
+        }
+
+        return shader;
+    }
+
+    public static createProgram(gl: WebGL2RenderingContext) {
+        const program = gl.createProgram()!,
+            vShader = this.createShader(gl, vertexShaderSource, gl.VERTEX_SHADER)!,
+            fShader = this.createShader(gl, fragmentShaderSource, gl.FRAGMENT_SHADER)!;
+
+        gl.attachShader(program, vShader);
+        gl.attachShader(program, fShader);
+        gl.linkProgram(program);
+
+        if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
+            console.error("Error creating shader program.", gl.getProgramInfoLog(program));
+            gl.deleteProgram(program);
+            return null;
+        }
+        // debugging
+        if (true) {
+            gl.validateProgram(program);
+            if (!gl.getProgramParameter(program, gl.VALIDATE_STATUS)) {
+                console.error("Error validating program.", gl.getProgramInfoLog(program));
+                gl.deleteProgram(program);
+                return null;
+            }
+        }
+
+        gl.detachShader(program, vShader);
+        gl.detachShader(program, fShader);
+        gl.deleteShader(vShader);
+        gl.deleteShader(fShader);
+
+        return program;
+    }
+}
